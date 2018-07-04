49c20b949c491292eadcd15c15411fb1e1691217
Joel Goodwin <joeldaniel@hotmail.co.uk>
2018-07-04 22:13:11 +0100

    DRY completed for cruise test js, Port test js still to be done

--- __tests__/cruise.test.js
+++ __tests__/cruise.test.js
@@ -2,74 +2,63 @@ const Ship = require('../src/cruise.js')
 const Port = require('../src/port.js')
 const Itinerary = require('../src/itinerary.js')
 
-describe('constructor', () => {
-  it('returns an object', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port])
-    const ship = new Ship(itinerary);
+describe('Ship', () => {
+  describe('with ports and an itinerary', () => {
+    let ship;
+    let dover;
+    let calais;
+    let itinerary;
 
-    expect(ship).toBeInstanceOf(Object);
+    beforeEach(() => {
+
+      dover = new Port('Dover');
+      calais = new Port('Calais')
+      itinerary = new Itinerary([dover, calais])
+      ship = new Ship(itinerary);
+
     });
 
+    it('returns an object', () => {
 
-  it('has a starting port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port])
-    const ship = new Ship(itinerary);
+      expect(ship).toBeInstanceOf(Object);
 
-    expect(ship.currentPort).toBe(port);
-  });
+      });
 
-  it('gets added to port on instination', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port])
-    const ship = new Ship(itinerary);
+    it('has a starting port', () => {
 
-    expect(port.ships).toContain(ship);
-  });
+      expect(ship.currentPort).toBe(dover);
+
+    });
+
+    it('gets added to port on instination', () => {
+
+      expect(dover.ships).toContain(ship);
+    });
   
-});
-
-describe('setSail', () => {
-  it('can set sail', () => {
-    const dover = new Port('Dover');
-    const calais = new Port('Calais')
-    const itinerary = new Itinerary([dover, calais])
-    const ship = new Ship(itinerary);
-
-    ship.setSail();
-
-    expect(ship.currentPort).toBeFalsy();
-    expect(dover.ships).not.toContain(ship);
-    });
-
+    it('can set sail', () => {
+  
+      ship.setSail();
+  
+      expect(ship.currentPort).toBeFalsy();
+      expect(dover.ships).not.toContain(ship);
+      });
+  
     it('can\'t sail further than itinerary', () => {
-      const dover = new Port('Dover');
-      const calais = new Port('Calais');
-      const itinerary = new Itinerary([dover, calais]);
-      const ship = new Ship(itinerary);
-  
+    
       ship.setSail()
       ship.dock();
-  
+    
       expect(() => ship.setSail()).toThrowError('End of itinerary reached');
-    });
+      });
 
-});
-
-describe('dock', () => {
-  it('can dock at a different port', () => {
-    const dover = new Port('Dover');
-    const calais = new Port('Calais');
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship(itinerary);
-
-    ship.setSail()
-    ship.dock();
-
-    expect(ship.currentPort).toBe(calais);
-    expect(calais.ships).toContain(ship);
-    });
-
+    it('can dock at a different port', () => {
+    
+      ship.setSail()
+      ship.dock();
+    
+      expect(ship.currentPort).toBe(calais);
+      expect(calais.ships).toContain(ship);
+      });
+  });
 });
 
